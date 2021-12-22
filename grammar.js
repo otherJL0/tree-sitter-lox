8ae175b0f2ee62d7e8ac92e7512e5a06c5f396a9
otherJL0 <jonathanglopez@gmail.com>
2021-12-22 11:57:42 -0500

    Adding support for binary expressions

--- grammar.js
+++ grammar.js
@@ -1,3 +1,28 @@
+const PREC = {
+  ASSIGN: 1, // =  += -=  *=  /=  %=  &=  ^=  |=  <<=  >>=  >>>=
+  SWITCH_EXP: 1, // always prefer to parse switch as expression over statement
+  DECL: 2,
+  ELEMENT_VAL: 2,
+  TERNARY: 3, // ?:
+  OR: 4, // ||
+  AND: 5, // &&
+  BIT_OR: 6, // |
+  BIT_XOR: 7, // ^
+  BIT_AND: 8, // &
+  EQUALITY: 9, // ==  !=
+  GENERIC: 10,
+  REL: 10, // <  <=  >  >=  instanceof
+  SHIFT: 11, // <<  >>  >>>
+  ADD: 12, // +  -
+  MULT: 13, // *  /  %
+  CAST: 14, // (Type)
+  OBJ_INST: 14, // new
+  UNARY: 15, // ++a  --a  a++  a--  +  -  !  ~
+  ARRAY: 16, // [Index]
+  OBJ_ACCESS: 16, // .
+  PARENS: 16, // (Expression)
+};
+
 module.exports = grammar({
   name: "lox",
 
@@ -22,6 +47,8 @@ module.exports = grammar({
         choice(
           $._assignment_statement,
           $._expression_statement,
+          $.if_statement,
+          $.while_statement,
           $.print_statement,
         ),
         ";",
@@ -32,19 +59,62 @@ module.exports = grammar({
 
     _assignment_statement: ($) =>
       seq(
-        "var",
+        optional("var"),
         $.identifier,
         "=",
         $._expression,
       ),
+
+    while_statement: ($) =>
+      seq(
+        "while",
+        field("condition", $._parenethesized_expression),
+        $.body,
+      ),
+
+    if_statement: ($) =>
+      seq(
+        "if",
+        field("condition", $._parenethesized_expression),
+        $.body,
+        optional(seq("else", $.body)),
+      ),
+    _parenethesized_expression: ($) => seq("(", repeat($._expression), ")"),
     _expression_statement: ($) => seq($._expression),
 
     _expression: ($) =>
       choice(
         $._literal,
         $.identifier,
+        $.binary_expression,
       ),
 
+    binary_expression: ($) =>
+      choice(
+        ...[
+          [">", PREC.REL],
+          ["<", PREC.REL],
+          [">=", PREC.REL],
+          ["<=", PREC.REL],
+          ["==", PREC.EQUALITY],
+          ["!=", PREC.EQUALITY],
+          ["and", PREC.AND],
+          ["or", PREC.OR],
+          ["+", PREC.ADD],
+          ["-", PREC.ADD],
+          ["*", PREC.MULT],
+          ["/", PREC.MULT],
+        ].map(([operator, precedence]) =>
+          prec.left(
+            precedence,
+            seq(
+              field("left", $._expression),
+              field("operator", operator),
+              field("right", $._expression),
+            ),
+          )
+        ),
+      ),
     _literal: ($) =>
       choice(
         $.number,
